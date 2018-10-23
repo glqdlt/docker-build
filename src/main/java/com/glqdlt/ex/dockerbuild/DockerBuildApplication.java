76b0bac03025fee3669a942b7c278ff54edb6f0b
전일웅 <jhun@nm-4ones.com>
2018-10-23 15:29:09 +0900

    init

---
+++ src/main/java/com/glqdlt/ex/dockerbuild/DockerBuildApplication.java
@@ -0,0 +1,12 @@
+package com.glqdlt.ex.dockerbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerBuildApplication.class, args);
+    }
+}
